7d27fb61fe423c7fad0a7f28c1e5c9f12e502511
Diego Solis <disc3110@gmail.com>
2021-11-17 14:44:08 -0600

    add final lines

--- lib/book.rb
+++ lib/book.rb
@@ -1,15 +1,15 @@
 require_relative 'rental'
 
- class Book
-   attr_accessor :title, :author, :rentals
+class Book
+  attr_accessor :title, :author, :rentals
 
-   def initialize(title, author)
-     @title = title
-     @author = author
-     @rentals = []
-   end
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
 
-   def add_rental(date, person)
-     Rental.new(date, self, person)
-   end
- end
+  def add_rental(date, person)
+    Rental.new(date, self, person)
+  end
+end
